f74a2a8fdbc926e5f2661c99bed980b255d4af36
Larissa Campos <laricastelar@gmail.com>
2022-10-29 16:30:34 -0400

    Added compose dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     }
 }
 
